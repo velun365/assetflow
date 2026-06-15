1bdf34a53683ac2fd13e75d99fcc957f161e88ab
velun365 <velun365@gmail.com>
2026-06-15 19:03:17 +0900

    init: create Spring Boot project

---
+++ src/test/java/com/assetflow/AssetflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
